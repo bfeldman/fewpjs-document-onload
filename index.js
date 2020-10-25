f89bb8ac634725e9b64c66e913ad3b5929ea008c
bfeldman <brian.feldman42@gmail.com>
2020-10-25 11:58:05 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", newMessage());
+
+function newMessage() {
+    element = document.getElementById('text')
+    element.innerHTML = "This is really cool!"
+}
